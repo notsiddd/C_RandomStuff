eb1db9526e66b979ab9ff0ab1cfdb0e7d68c5f94
notsiddd <86549996+notsiddd@users.noreply.github.com>
2022-10-02 14:51:34 +0530

    Update float_addition.c

--- float_addition.c
+++ float_addition.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("%f", 0.000001 + 0.000001 + 0.000001);
+int main(int argc, char** argv){
+    float inc = 0.000000001;
+    float n = 0.00000001; 
+    int c = 1;
+
+    for (float i=0.0; i<n; i+=inc) {
+        printf("Loop Number: %d\n", c);
+        c++;
+    }
 
     return 0;
 }
+
